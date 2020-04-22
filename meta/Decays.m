154e20ef57b598855a561ff83254409d4a8a6338
Wojciech Kotlarski <wojciech.kotlarski@tu-dresden.de>
2020-04-23 00:21:34 +0200

    fix test_flexiblesusy-config
    moved Higgs decays to SM particles routines from templates to src

--- meta/Decays.m
+++ meta/Decays.m
@@ -2000,7 +2000,7 @@ CreateIncludedPartialWidthSpecialization[decay_FSParticleDecay, modelName_] :=
     Module[{initialParticle = GetInitialState[decay], finalState = GetFinalState[decay],
             declaration = "", includeStatement = ""},
            declaration = CreatePartialWidthSpecializationDecl[decay, modelName];
-           includeStatement = "#include \"templates/sm_h_decays/decay_" <>
+           includeStatement = "#include \"H_SM_decays/decay_" <>
                               SimplifiedName[initialParticle] <> "_to_" <>
                               StringJoin[SimplifiedName[# /. SARAH`bar|Susyno`LieGroups`conj -> Identity]& /@ finalState] <>
                               ".cpp\"";
