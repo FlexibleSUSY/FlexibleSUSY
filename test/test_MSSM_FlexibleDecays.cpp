0908894ed330f3425290272c64d8b79e818eb469
Wojciech Kotlarski <wojciech.kotlarski@tu-dresden.de>
2020-04-23 04:11:02 +0200

    reduced precision of FlexibleDecay test in the MSSM

--- test/test_MSSM_FlexibleDecays.cpp
+++ test/test_MSSM_FlexibleDecays.cpp
@@ -648,13 +648,13 @@ Block MSOFT Q= 8.61574711E+02
 
    // h -> b bbar
    BOOST_CHECK_CLOSE_FRACTION(decays.partial_width_hh_to_barFdFd(&m, 0, 2, 2),
-                              0.0016298093732297074, 1e-14);
+                              0.0016298093732297074, 6e-14);
    // h -> tau+ tau-
    BOOST_CHECK_CLOSE_FRACTION(decays.partial_width_hh_to_barFeFe(&m, 0, 2, 2),
-                              0.0002600448772469762, 1e-14);
+                              0.0002600448772469762, 6e-14);
    // h -> W+ W-
    BOOST_CHECK_CLOSE_FRACTION(decays.partial_width_hh_to_conjVWmVWm(&m, 0),
-                              0.00022767000236269188, 3e-13);
+                              0.00022767000236269188, 8e-13);
    // h -> Z Z
    BOOST_CHECK_CLOSE_FRACTION(decays.partial_width_hh_to_VZVZ(&m, 0),
                               1.6116438957094875e-05, 2e-12);
@@ -662,9 +662,9 @@ Block MSOFT Q= 8.61574711E+02
    // ------------ loop-induces decays ------------
 
    // h -> gluon gluon
-   BOOST_CHECK_CLOSE_FRACTION(decays.partial_width_hh_to_VGVG(&m, 0), 0.00015442075031863592, 6e-14);
+   BOOST_CHECK_CLOSE_FRACTION(decays.partial_width_hh_to_VGVG(&m, 0), 0.00015442075031863592, 3e-13);
    // h -> gamma gamma
-   BOOST_CHECK_CLOSE_FRACTION(decays.partial_width_hh_to_VPVP(&m, 0), 6.5333738573774704e-06, 1e-13);
+   BOOST_CHECK_CLOSE_FRACTION(decays.partial_width_hh_to_VPVP(&m, 0), 6.5333738573774704e-06, 4e-13);
    // h -> gamma Z
    BOOST_CHECK_CLOSE_FRACTION(decays.partial_width_hh_to_VPVZ(&m, 0), 3.2717635916980618e-06, 1e-12);
 
