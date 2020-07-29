41c42bd615d19fd99a48744947feaeec4998fe14
Wojciech Kotlarski <wojciech.kotlarski@tu-dresden.de>
2020-07-29 20:21:47 +0200

    3-loop QCD corrections to Ah->gg

--- src/H_SM_decays/decay_AH_to_GG.cpp
+++ src/H_SM_decays/decay_AH_to_GG.cpp
@@ -34,24 +34,18 @@ double CLASSNAME::get_partial_width<AH, G, G>(
 
       const double deltaNLO {97./4. - 7./6.*Nf};
 
-      /*
       const double log_mAh2OverMT2 {std::log(Sqr(mAh/mtpole))};
 
-      const double mtpole2 {Sqr(mtpole)};
       const double deltaNNLO {
-        149533./288. - 363./8.*zeta2 - 495./8.*zeta3 + 19./8.*log_mAh2OverMT2
-              + Nf*(-4157./72 + 11./2.*zeta2 + 5./4.*zeta3 + 2./3.*log_mAh2OverMT2)
-              + Nf*Nf*(127./108. - zeta2/6.)
+         237311./864. - 529./24.*zeta2 - 445./8.*zeta3 + 5.*log_mAh2OverMT2
       };
-      const double deltaNNNLO {467.683620788 + 122.440972222*log_mAh2OverMT2 + 10.9409722222*Sqr(log_mAh2OverMT2)};
-      */
 
       const double g3 = context.model.get_g3();
       const double alpha_s_red = Sqr(g3)/(4*Sqr(Pi));
 
       switch (include_higher_order_corrections) {
          case SM_higher_order_corrections::enable:
-            result *= 1. + deltaNLO*alpha_s_red /*+ deltaNNLO*Sqr(alpha_s_red) + deltaNNNLO*Cube(alpha_s_red)*/;
+            result *= 1. + deltaNLO*alpha_s_red + deltaNNLO*Sqr(alpha_s_red) /*+ deltaNNNLO*Cube(alpha_s_red)*/;
             break;
          case SM_higher_order_corrections::disable:
             break;
