4ae819be6ee2dd99f33e5ed7705989edf84df51b
Wojciech Kotlarski <wojciech.kotlarski@tu-dresden.de>
2021-03-22 13:19:06 +0100

    missing space

--- src/decays/H_SM_decays/decay_H_to_WW.cpp
+++ src/decays/H_SM_decays/decay_H_to_WW.cpp
@@ -90,7 +90,7 @@ double CLASSNAME::get_partial_width<H, conj<W>::type, W>(
       // matrix element squared
       const auto mat_elem = calculate_amplitude<H, conj<W>::type, W>(
          context, indexIn, indexOut1, indexOut2);
-      const auto mat_elem_sq =mat_elem.square();
+      const auto mat_elem_sq = mat_elem.square();
 
       // flux * phase space factor * matrix element squared
       res = flux * ps * mat_elem_sq;
