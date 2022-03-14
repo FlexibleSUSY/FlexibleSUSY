afc16a871d392fe7767c4f58bcd6d4123805de90
Alexander Voigt <ahgvoigt@gmail.com>
2022-03-14 09:23:52 +0100

    update Higgs pole mass to PDG 2021 (update 2022)

--- src/ew_input.hpp
+++ src/ew_input.hpp
@@ -44,7 +44,7 @@ namespace Electroweak_constants {
       constexpr double Error_MZ = 0.0021; ///< uncertainty on MZ from PDG
       constexpr double MW = 80.385;
       constexpr double MWSM = 80.357; ///< SM prediction from arXiv:1411.7040
-      constexpr double MH = 125.09; ///< Higgs mass from PDG (CMS and ATLAS combination)
+      constexpr double MH = 125.25; ///< Higgs mass from PDG 2022 update
       constexpr double Error_MH = 0.24; ///< uncertainty on MH from PDG - 0.11 (sys) and 0.21 stat combined in quadrature.
       constexpr double MUP = 2.4e-3; ///< default running quark mass from PDG
       constexpr double MDOWN = 4.75e-3; ///< default running quark mass from PDG
