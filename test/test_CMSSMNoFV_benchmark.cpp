4d475e301364138a3e6c020964936302a5d9a959
Wojciech Kotlarski <wojciech.kotlarski@tu-dresden.de>
2022-03-20 13:04:51 +0100

    removed check comparing FS and SS runtimes

--- test/test_CMSSMNoFV_benchmark.cpp
+++ test/test_CMSSMNoFV_benchmark.cpp
@@ -141,8 +141,6 @@ void test_tanbeta_scan()
         "  Softsusy    : " << ss_average_time <<
         " (" << ss_data.number_of_valid_points << "/" << num_points << " points)\n" <<
         "  FlexibleSUSY speed: " << (100.*(fs_average_time - ss_average_time)/ss_average_time) << "%");
-
-   TEST_GREATER(ss_average_time, fs_average_time);
 }
 
 int main()
