6aff216a4865039d54428c23e74574a4ccef270a
Alexander Voigt <ahgvoigt@gmail.com>
2022-09-14 07:41:54 +0200

    formatting

--- test/test_standard_model_G_fermi.cpp
+++ test/test_standard_model_G_fermi.cpp
@@ -1,4 +1,3 @@
-
 #define BOOST_TEST_DYN_LINK
 #define BOOST_TEST_MODULE test_standard_model_G_fermi
 
@@ -12,24 +11,21 @@
 #include "standard_model.hpp"
 #include "lowe.h"
 
-
 using namespace flexiblesusy;
-
 
 BOOST_AUTO_TEST_CASE( test_G_fermi )
 {
    standard_model::Standard_model sm;
    sm.set_pole_mass_loop_order(0);
    sm.set_ewsb_loop_order(0);
-  
+
    softsusy::QedQcd qedqcd;
    sm.initialise_from_input(qedqcd);
    sm.calculate_DRbar_masses();
 
-
    const double G_fermi_input = qedqcd.displayFermiConstant();
    const double G_fermi_pred  = sm.calculate_G_fermi(qedqcd);
-   
+
    const double gY                  = sm.get_g1() * 0.7745966692414834;
    const double g2                  = sm.get_g2();
    const double e                   = gY*g2 / Sqrt(Sqr(gY) + Sqr(g2));
@@ -39,16 +35,11 @@ BOOST_AUTO_TEST_CASE( test_G_fermi )
    const double sin_theta           = Sin(theta_w);
    const double MZ2pole             = Sqr(qedqcd.displayPoleMZ());
    const double sqrt2               = 1.4142135623730950;
-   const double Pi                  = 3.1415926535897932; 
+   const double Pi                  = 3.1415926535897932;
 
-   const double G_fermi_0l    = Pi * alpha_em_drbar / (AbsSqr(sin_theta*cos_theta) * sqrt2 * MZ2pole );
+   const double G_fermi_0l = Pi * alpha_em_drbar / (AbsSqr(sin_theta*cos_theta) * sqrt2 * MZ2pole);
 
    BOOST_CHECK_CLOSE_FRACTION(G_fermi_input, G_fermi_0l, 1.0e-2);
- 
+
    BOOST_CHECK_CLOSE_FRACTION(G_fermi_input, G_fermi_pred, 1.0e-5);
-
-
 }
-
-
-
