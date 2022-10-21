bf879f6bbf41687b9c48645e22408a67f6bb7f21
Alexander Voigt <ahgvoigt@gmail.com>
2022-10-21 18:56:44 +0200

    remove MS scan

--- test/test_NUHMSSMNoFVHimalaya.cpp
+++ test/test_NUHMSSMNoFVHimalaya.cpp
@@ -34,10 +34,12 @@ NUHMSSMNoFVHimalaya<Two_scale> run(int loops, const NUHMSSMNoFVHimalaya_input_pa
 
    Spectrum_generator_settings settings;
    settings.set(Spectrum_generator_settings::precision, 1.0e-5);
+   settings.set(Spectrum_generator_settings::max_iterations, 10000);
    settings.set(Spectrum_generator_settings::pole_mass_loop_order, loops);
    settings.set(Spectrum_generator_settings::ewsb_loop_order, loops);
    settings.set(Spectrum_generator_settings::beta_loop_order, 3);
    settings.set(Spectrum_generator_settings::threshold_corrections_loop_order, loops);
+   settings.set(Spectrum_generator_settings::force_output, true); // must force the output, because xt = sqrt(6) leads to tachyons
    settings.set_threshold_corrections(tc);
 
    NUHMSSMNoFVHimalaya_spectrum_generator<Two_scale> spectrum_generator;
@@ -59,14 +61,14 @@ double calc_Mh(int loops, double tb, double MS, double xt)
    input.M2 = MS;
    input.M3 = MS;
    input.AtIN = Xt + mu/tb;
-   input.AbIN = 0;
-   input.AtauIN = 0;
-   input.AcIN = 0;
-   input.AsIN = 0;
-   input.AmuonIN = 0;
-   input.AuIN = 0;
-   input.AdIN = 0;
-   input.AeIN = 0;
+   input.AbIN = mu*tb;
+   input.AtauIN = mu*tb;
+   input.AcIN = mu/tb;
+   input.AsIN = mu*tb;
+   input.AmuonIN = mu*tb;
+   input.AuIN = mu/tb;
+   input.AdIN = mu*tb;
+   input.AeIN = mu*tb;
    input.MuIN = mu;
    input.mA2IN = MS*MS;
    input.ml11IN = MS;
@@ -89,41 +91,14 @@ double calc_Mh(int loops, double tb, double MS, double xt)
    return run(loops, input).get_physical().Mhh(0);
 }
 
-struct Mh {
-   double MS{}, Mh0L{}, Mh1L{}, Mh2L{}, Mh3L{};
-};
-
 /// test some points from arxiv:1708.05720 Fig.6
 BOOST_AUTO_TEST_CASE( test_Mh )
 {
-   /*
-   std::vector<Mh> data;
-
-   const double tb = 5;
-   const double xt = -std::sqrt(6.0);
-   const double MS_start = 1000;
-   const double MS_stop = 10000;
-   const int N = 9;
-
-   for (int i = 0; i <= N; i++) {
-      const double MS = MS_start + i*(MS_stop - MS_start)/N;
-      data.push_back({
-            MS,
-            calc_Mh(0, tb, MS, xt),
-            calc_Mh(1, tb, MS, xt),
-            calc_Mh(2, tb, MS, xt),
-            calc_Mh(3, tb, MS, xt)
-         });
-   }
-
-   std::printf("# tb = %f, xt = %f\n", tb,  xt);
-   std::printf("# %5s %7s %7s %7s %7s\n", "MS",  "Mh0L", "Mh1L", "Mh2L", "Mh3L");
-
-   for (const auto& d: data) {
-      std::printf("%7.3f %7.3f %7.3f %7.3f %7.3f\n", d.MS,  d.Mh0L, d.Mh1L, d.Mh2L, d.Mh3L);
-   }
-   */
-
    BOOST_CHECK_CLOSE_FRACTION(calc_Mh(2, 5, 1e4, -std::sqrt(6.0)), 127.25093812326084, 1e-4);
+   // BOOST_CHECK_CLOSE_FRACTION(calc_Mh(2, 5, 1e4, 0), 120.58946229887117, 1e-4);
+   // BOOST_CHECK_CLOSE_FRACTION(calc_Mh(2, 5, 1e4, std::sqrt(6.0)), 130.0526030176658, 1e-4);
+
    BOOST_CHECK_CLOSE_FRACTION(calc_Mh(3, 5, 1e4, -std::sqrt(6.0)), 124.66777554988728, 2e-4);
+   BOOST_CHECK_CLOSE_FRACTION(calc_Mh(3, 5, 1e4, 0), 118.99014138640648, 2e-4);
+   BOOST_CHECK_CLOSE_FRACTION(calc_Mh(3, 5, 1e4, std::sqrt(6.0)), 126.94917275718093, 2e-4);
 }
