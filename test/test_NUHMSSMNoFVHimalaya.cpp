22ec5d179410a0af9e4908858382209cbe301342
Alexander Voigt <ahgvoigt@gmail.com>
2022-10-21 07:20:25 +0200

    make test run (loop orders missing)

--- test/test_NUHMSSMNoFVHimalaya.cpp
+++ test/test_NUHMSSMNoFVHimalaya.cpp
@@ -5,6 +5,10 @@
 
 #include "models/NUHMSSMNoFVHimalaya/NUHMSSMNoFVHimalaya_two_scale_spectrum_generator.hpp"
 
+#include <cstdio>
+#include <vector>
+#include <tuple>
+
 using namespace flexiblesusy;
 
 NUHMSSMNoFVHimalaya<Two_scale> run(const NUHMSSMNoFVHimalaya_input_parameters& input)
@@ -25,31 +29,74 @@ NUHMSSMNoFVHimalaya<Two_scale> run(const NUHMSSMNoFVHimalaya_input_parameters& i
 double calc_Mh(int loops, double tb, double MS, double xt)
 {
    NUHMSSMNoFVHimalaya_input_parameters input;
+   const double mu = MS;
+   const double Xt = xt*MS;
+
+   input.TanBeta = tb;
+   input.Qin = MS;
+   input.M1 = MS;
+   input.M2 = MS;
+   input.M3 = MS;
+   input.AtIN = Xt + mu/tb;
+   input.AbIN = 0;
+   input.AtauIN = 0;
+   input.AcIN = 0;
+   input.AsIN = 0;
+   input.AmuonIN = 0;
+   input.AuIN = 0;
+   input.AdIN = 0;
+   input.AeIN = 0;
+   input.MuIN = mu;
+   input.mA2IN = MS*MS;
+   input.ml11IN = MS;
+   input.ml22IN = MS;
+   input.ml33IN = MS;
+   input.me11IN = MS;
+   input.me22IN = MS;
+   input.me33IN = MS;
+   input.mq11IN = MS;
+   input.mq22IN = MS;
+   input.mq33IN = MS;
+   input.mu11IN = MS;
+   input.mu22IN = MS;
+   input.mu33IN = MS;
+   input.md11IN = MS;
+   input.md22IN = MS;
+   input.md33IN = MS;
+   input.Mlow = 0;
 
    return run(input).get_physical().Mhh(0);
 }
 
+struct Mh {
+   double MS{}, Mh0L{}, Mh1L{}, Mh2L{}, Mh3L{};
+};
+
 BOOST_AUTO_TEST_CASE( test_Mh )
 {
+   std::vector<Mh> data;
+
    const double tb = 20;
    const double xt = -std::sqrt(6.0);
    const double MS_start = 500;
    const double MS_stop = 3000;
-   const int N = 50;
+   const int N = 10;
 
    for (int i = 0; i <= N; i++) {
       const double MS = MS_start + i*(MS_stop - MS_start)/N;
-      const std::tuple<double, double, double, double> Mh{
-         calc_Mh(0, tb, MS, xt),
-         calc_Mh(1, tb, MS, xt),
-         calc_Mh(2, tb, MS, xt),
-         calc_Mh(3, tb, MS, xt)
-      };
+      data.push_back({
+            MS,
+            calc_Mh(0, tb, MS, xt),
+            calc_Mh(1, tb, MS, xt),
+            calc_Mh(2, tb, MS, xt),
+            calc_Mh(3, tb, MS, xt)
+         });
    }
 
-   // BOOST_CHECK_GT(std::abs(mw1/calc_mw_SM(mh1) - 1), 1.0e-5);
-   // BOOST_CHECK_CLOSE_FRACTION(mw1, calc_mw_SM(mh1), 1.0e-4);
-   // BOOST_CHECK_CLOSE_FRACTION(mw2, calc_mw_SM(mh2), 1.0e-4);
-   // BOOST_CHECK_CLOSE_FRACTION(mw5, calc_mw_SM(mh5), 1.0e-5);
-   // BOOST_CHECK_CLOSE_FRACTION(mw10, calc_mw_SM(mh10), 1.0e-6);
+   std::printf("# tb = %f, xt = %f\n", tb,  xt);
+   std::printf("# %5s %7s %7s %7s %7s\n", "MS",  "Mh0L", "Mh1L", "Mh2L", "Mh3L");
+
+   for (const auto& d: data) {
+      std::printf("%7.3f %7.3f %7.3f %7.3f %7.3f\n", d.MS,  d.Mh0L, d.Mh1L, d.Mh2L, d.Mh3L);
+   }
 }
