705af825fbf602a51846f47de9650ed9224c7a0f
Alexander Voigt <ahgvoigt@gmail.com>
2023-05-07 12:54:44 +0200

    relax test precision
    due to a numerical instability from re-ordering mass-degenerate
    sfermion masses.

--- test/test_CMSSMCPV_edm.cpp
+++ test/test_CMSSMCPV_edm.cpp
@@ -639,12 +639,14 @@ Block UURMIX
 
    using CMSSMCPV_cxx_diagrams::fields::Fe;
 
+   const double eps = 1e-11;
+
    auto de = CMSSMCPV_edm::calculate_edm<Fe>(m, qedqcd, 0);
-   BOOST_CHECK_CLOSE_FRACTION(de, -3.2103128259718866e-13, 3e-13);
+   BOOST_CHECK_CLOSE_FRACTION(de, -3.2103128259718866e-13, eps);
 
    auto dmu = CMSSMCPV_edm::calculate_edm<Fe>(m, qedqcd, 1);
-   BOOST_CHECK_CLOSE_FRACTION(dmu, -6.6380036570347626e-11, 2e-13);
+   BOOST_CHECK_CLOSE_FRACTION(dmu, -6.6380036570347626e-11, eps);
 
    auto dtau = CMSSMCPV_edm::calculate_edm<Fe>(m, qedqcd, 2);
-   BOOST_CHECK_CLOSE_FRACTION(dtau, -1.1209226809367614e-09, 3e-13);
+   BOOST_CHECK_CLOSE_FRACTION(dtau, -1.1209226809367614e-09, eps);
 }
