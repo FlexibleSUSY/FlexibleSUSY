9afceb526ee1e196a84638a9a5ce947ed533d235
Wojciech Kotlarski <wojciech.kotlarski@posteo.net>
2023-06-21 10:08:26 +0200

    cleanup

--- test/test_standard_model_mw_calculation.cpp
+++ test/test_standard_model_mw_calculation.cpp
@@ -1,6 +1,6 @@
 
 #define BOOST_TEST_DYN_LINK
-#define BOOST_TEST_MODULE test_SM_mw_calculation
+#define BOOST_TEST_MODULE test_standard_model_mw_calculation
 
 #include <boost/test/unit_test.hpp>
 
@@ -9,9 +9,6 @@
 #include "lowe.h"
 #include "ew_input.hpp"
 #include "standard_model.hpp"
-//#include "SM_input_parameters.hpp"
-//#include "SM_two_scale_spectrum_generator.hpp"
-//#include "ew_input.hpp"
 #include "sm_mw.hpp"
 
 using namespace flexiblesusy;
@@ -34,9 +31,6 @@ BOOST_AUTO_TEST_CASE( test_consistency )
    softsusy::QedQcd qedqcd;
    standard_model::Standard_model sm{};
    sm.initialise_from_input(qedqcd);
-   sm.solve_ewsb_tree_level();
-   sm.calculate_DRbar_masses();
-   sm.solve_ewsb();
    sm.calculate_pole_masses();
    const double mw = sm.get_physical().MVWp;
    const double mh = sm.get_physical().Mhh;
