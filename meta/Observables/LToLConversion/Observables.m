42844e41ae5f009d6d74e1c3ddf9b5360dc4a2cd
Uladzimir Khasianevich <khasianevich.u.u@gmail.com>
2023-12-19 12:34:38 +0100

    better description

--- meta/Observables/LToLConversion/Observables.m
+++ meta/Observables/LToLConversion/Observables.m
@@ -6,5 +6,5 @@ DefineObservable[
       "calculate_leplep_forcontr_loopNloop($(iI-1), $(iO-1), context::Nucleus::nucl, model, ltolconversion_settings, qedqcd)",
    GetObservablePrototype   ->
       "calculate_leplep_forcontr_loopNloop(int in, int out, const context::Nucleus n, auto model, const LToLConversion_settings& parameters, auto qedqcd)",
-   GetObservableDescription -> "lep(iI)->lep(iO) in nucl for contr at loopN loop"
+   GetObservableDescription -> "CR(lep(iI)->lep(iO)) in nucl for contr at loopN loop"
 ];
