321afbd0a2c02532d2e9a24ce4057567bb70de45
Alexander Voigt <alexander.voigt@physik.rwth-aachen.de>
2024-02-08 13:42:21 +0100

    extract test for Yukawa sign convention

--- test/module.mk
+++ test/module.mk
@@ -554,6 +554,7 @@ TEST_SRC += \
 		$(DIR)/test_SM_two_loop_spectrum.cpp \
 		$(DIR)/test_SM_three_loop_spectrum.cpp \
 		$(DIR)/test_SM_mw_calculation.cpp \
+		$(DIR)/test_SM_yukawa_convention.cpp \
 		$(DIR)/test_standard_model_weinberg_angle.cpp
 TEST_SH += \
 		$(DIR)/test_SM_observable_problems.sh
@@ -1220,6 +1221,8 @@ $(DIR)/test_SM_three_loop_spectrum.x: $(LIBSM)
 $(DIR)/test_SM_two_loop_spectrum.x: $(LIBSM)
 
 $(DIR)/test_SM_mw_calculation.x: $(LIBSM)
+
+$(DIR)/test_SM_yukawa_convention.x: $(LIBSM)
 
 $(DIR)/test_SM_cxxvertices.x: $(LIBSM)
 
