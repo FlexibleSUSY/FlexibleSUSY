6e875c0ffa73c5fbf871b43f8e61704643e80209
Uladzimir Khasianevich <khasianevich.u.u@gmail.com>
2024-02-08 12:01:05 +0100

    changing example according to article

--- examples/new-observable/fermion-mass-observable/ExampleFermionMass/Observables.m
+++ examples/new-observable/fermion-mass-observable/ExampleFermionMass/Observables.m
@@ -2,5 +2,5 @@ Observables`DefineObservable[
    FlexibleSUSYObservable`ExampleFermionMass[fermion_@gen_],
    GetObservableType        -> {2},
    GetObservablePrototype   -> "ex_fermion_mass(int gen, auto model, auto qedqcd)",
-   GetObservableDescription -> "fermion[gen] (lepton[gen] for ExampleLeptonMass) mass"
+   GetObservableDescription -> "fermion[gen] (lepton[gen] if in Block ExampleLeptonMass) mass"
 ];
