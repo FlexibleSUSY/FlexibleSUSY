215971fe94edcfa7195cc3c9359b08f5436e64f0
Alexander Voigt <Expander@users.noreply.github.com>
2024-03-24 21:19:09 +0100

    Update polylogarithms and Clausen function (#615)
    from polylogarithm library 7.0.0
    
    This new version fixes a numerical imprecision in the complex
    polylogarithms for small (complex) arguments.

--- src/horner.hpp
+++ src/horner.hpp
@@ -16,18 +16,43 @@
 // <http://www.gnu.org/licenses/>.
 // ====================================================================
 
-#ifndef LI5_H
-#define LI5_H
+#ifndef HORNER_H
+#define HORNER_H
 
 #include <complex>
 
 namespace flexiblesusy {
 
-/// complex polylogarithm with n=5
-std::complex<double> Li5(const std::complex<double>&) noexcept;
+template <typename T, int N>
+T horner(T x, const T (&c)[N]) noexcept
+{
+   T p = c[N - 1];
+   for (int i = N - 2; i >= 0; --i) {
+      p = p*x + c[i];
+   }
+   return p;
+}
 
-/// complex polylogarithm with n=5 with long double precision
-std::complex<long double> Li5(const std::complex<long double>&) noexcept;
+
+template <int Nstart, typename T, int N>
+std::complex<T> horner(const std::complex<T>& z, const T (&coeffs)[N]) noexcept
+{
+   static_assert(0 <= Nstart && Nstart < N && N >= 2, "invalid array bounds");
+
+   const T rz = std::real(z);
+   const T iz = std::imag(z);
+   const T r = rz + rz;
+   const T s = std::norm(z);
+   T a = coeffs[N - 1], b = coeffs[N - 2];
+
+   for (int i = N - 3; i >= Nstart; --i) {
+      const T t = a;
+      a = b + r*a;
+      b = coeffs[i] - s*t;
+   }
+
+   return { rz*a + b, iz*a };
+}
 
 } // namespace flexiblesusy
 
