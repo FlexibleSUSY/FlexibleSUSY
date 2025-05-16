21fbaebbcfef835da92836733a8f6084a98d0860
Wojciech Kotlarski <wojciech.kotlarski@posteo.net>
2025-05-16 10:35:37 +0200

    test all versions

--- test/test_LQS1_FlexibleDecay.cpp
+++ test/test_LQS1_FlexibleDecay.cpp
@@ -36,26 +36,26 @@ BOOST_AUTO_TEST_CASE( test_SM_FlexibleDecay )
    input.MS12Input = 2250000;
    input.gHS1Input = 0.1;
 
-   LQS1_up_basis<Two_scale> _m;
-   setup_LQS1_up_basis_const(_m, input);
+   LQS1_up_basis<Two_scale> m_;
+   setup_LQS1_up_basis_const(m_, input);
 
-   _m.calculate_DRbar_masses();
+   m_.calculate_DRbar_masses();
 
-   _m.set_pole_mass_loop_order(1);
-   _m.do_calculate_sm_pole_masses(true);
-   _m.solve_ewsb_one_loop();
-   _m.calculate_pole_masses();
+   m_.set_pole_mass_loop_order(1);
+   m_.do_calculate_sm_pole_masses(true);
+   m_.solve_ewsb_one_loop();
+   m_.calculate_pole_masses();
 
-   if (_m.get_problems().have_problem()) {
+   if (m_.get_problems().have_problem()) {
       std::ostringstream ostr;
-      _m.get_problems().print_problems(ostr);
+      m_.get_problems().print_problems(ostr);
       BOOST_FAIL(ostr.str());
    }
 
    softsusy::QedQcd qedqcd;
    Physical_input physical_input;
    FlexibleDecay_settings flexibledecay_settings;
-   LQS1_up_basis_mass_eigenstates_running m(_m);
+   LQS1_up_basis_mass_eigenstates_running m(m_);
 
    m.set_scale(100.0);
 
@@ -93,60 +93,35 @@ BOOST_AUTO_TEST_CASE( test_SM_FlexibleDecay )
    std::cout  << " v: " << m.get_v() << '\n';
    std::cout  << " TW: " << m.ThetaW() << '\n';
 
-   // -----------------------------------------------------
-   // decays with higher-order SM corrections
-
-   LQS1_up_basis_decays decays_HO = LQS1_up_basis_decays(m, qedqcd, physical_input, flexibledecay_settings);
-   decays_HO.sm.initialise_from_input(qedqcd);
-   decays_HO.sm.set_loops(m.get_loops());
-   decays_HO.sm.solve_ewsb_tree_level();
-   decays_HO.sm.calculate_DRbar_masses();
-   decays_HO.sm.calculate_pole_masses();
-   decays_HO.sm_decays = Standard_model_decays(decays_HO.sm, qedqcd, physical_input, flexibledecay_settings);
-
-   // ------------ tree-level decays ------------
-
-   // h -> b bbar
-   //BOOST_CHECK_CLOSE_FRACTION(decays_HO.partial_width_hh_to_barFdFd(&m, 2, 2), 0.0023811031255194888, 2e-15);
-   // h -> c cbar
-   //BOOST_CHECK_CLOSE_FRACTION(decays_HO.partial_width_hh_to_barFuFu(&m, 1, 1), 0.00011737301687946969, 2e-16);
-   // h -> mu+ mu-
-   BOOST_CHECK_CLOSE_FRACTION(decays_HO.partial_width_hh_to_barFeFe(&m, 1, 1), 5.6230325675374227851e-7, 1e-15);
-   // h -> W+ W-
-   //BOOST_CHECK_CLOSE_FRACTION(decays_HO.partial_width_hh_to_VWpconjVWp(&m), 0.00096256841980060836, 1e-3);
-   // h -> Z Z
-   //BOOST_CHECK_CLOSE_FRACTION(decays_HO.partial_width_hh_to_VZVZ(&m), 0.00010568799794141996, 1e-3);
-
-   // ------------ loop-induces decays_HO ------------
-
-   // h -> gluon gluon
-   //BOOST_CHECK_CLOSE_FRACTION(decays_HO.partial_width_hh_to_VGVG(&m), 0.00035462447439152465, 5e-13);
-   // h -> gamma gamma
-   //BOOST_CHECK_CLOSE_FRACTION(decays_HO.partial_width_hh_to_VPVP(&m), 9.2117697375801348e-06, 2e-13);
-   // h -> gamma Z
-   //BOOST_CHECK_CLOSE_FRACTION(decays_HO.partial_width_hh_to_VPVZ(&m), 6.106402229544854e-06, 2e-13);
-
-   // -----------------------------------------------------
-   // decays without higher-order SM corrections
-
+   // tree-level
    flexibledecay_settings.set(FlexibleDecay_settings::include_higher_order_corrections, 0.0);
-   LQS1_up_basis_decays decays_no_HO = LQS1_up_basis_decays(m, qedqcd, physical_input, flexibledecay_settings);
+   LQS1_up_basis_decays decays_noHO = LQS1_up_basis_decays(m, qedqcd, physical_input, flexibledecay_settings);
+   BOOST_CHECK_CLOSE_FRACTION(decays_noHO.partial_width_hh_to_barFeFe(&m, 1, 1), 5.6230325675374227851e-7, 1e-15);
 
-   // ------------ tree-level decays ------------
+   // 1l QED
+   flexibledecay_settings.set(FlexibleDecay_settings::include_higher_order_corrections, 10.0);
+   LQS1_up_basis_decays decays_HO_SM = LQS1_up_basis_decays(m, qedqcd, physical_input, flexibledecay_settings);
+   BOOST_CHECK_CLOSE_FRACTION(decays_HO_SM.partial_width_hh_to_barFeFe(&m, 1, 1), 5.6230325675374227851e-7, 1e-15);
 
-   // h -> b bbar
-   //BOOST_CHECK_CLOSE_FRACTION(decays_no_HO.partial_width_hh_to_barFdFd(&m, 2, 2), 0.00207153178400001, 2e-15);
-   // h -> c cbar
-   //BOOST_CHECK_CLOSE_FRACTION(decays_no_HO.partial_width_hh_to_barFuFu(&m, 1, 1), 7.4579299427674489e-06, 1e-16);
-   // h -> mu+ mu-
-   BOOST_CHECK_CLOSE_FRACTION(decays_no_HO.partial_width_hh_to_barFeFe(&m, 1, 1), 9.077062783486042e-7, 1e-15);
+   // 1l BSM
+   flexibledecay_settings.set(FlexibleDecay_settings::include_higher_order_corrections, 1.0);
+   LQS1_up_basis_decays decays_HO_BSM = LQS1_up_basis_decays(m, qedqcd, physical_input, flexibledecay_settings);
+   decays_HO_BSM.sm.initialise_from_input(qedqcd);
+   decays_HO_BSM.sm.set_loops(m.get_loops());
+   decays_HO_BSM.sm.solve_ewsb_tree_level();
+   decays_HO_BSM.sm.calculate_DRbar_masses();
+   decays_HO_BSM.sm.calculate_pole_masses();
+   decays_HO_BSM.sm_decays = Standard_model_decays(decays_HO_BSM.sm, qedqcd, physical_input, flexibledecay_settings);
+   BOOST_CHECK_CLOSE_FRACTION(decays_HO_BSM.partial_width_hh_to_barFeFe(&m, 1, 1), 5.6230325675374227851e-7, 1e-15);
 
-   // ------------ loop-induces decays_no_HO ------------
-
-   // h -> gluon gluon
-   //BOOST_CHECK_CLOSE_FRACTION(decays_no_HO.partial_width_hh_to_VGVG(&m), 0.00019973935357968308, 5e-13);
-   // h -> gamma gamma
-   //BOOST_CHECK_CLOSE_FRACTION(decays_no_HO.partial_width_hh_to_VPVP(&m), 9.0492996379713826e-06, 2e-13);
-   // h -> gamma Z
-   //BOOST_CHECK_CLOSE_FRACTION(decays_no_HO.partial_width_hh_to_VPVZ(&m), 6.0803965974442744e-06, 2e-13);
+   // 1l SM+BSM
+   flexibledecay_settings.set(FlexibleDecay_settings::include_higher_order_corrections, 11.0);
+   LQS1_up_basis_decays decays_HO_SM_BSM = LQS1_up_basis_decays(m, qedqcd, physical_input, flexibledecay_settings);
+   decays_HO_SM_BSM.sm.initialise_from_input(qedqcd);
+   decays_HO_SM_BSM.sm.set_loops(m.get_loops());
+   decays_HO_SM_BSM.sm.solve_ewsb_tree_level();
+   decays_HO_SM_BSM.sm.calculate_DRbar_masses();
+   decays_HO_SM_BSM.sm.calculate_pole_masses();
+   decays_HO_SM_BSM.sm_decays = Standard_model_decays(decays_HO_SM_BSM.sm, qedqcd, physical_input, flexibledecay_settings);
+   BOOST_CHECK_CLOSE_FRACTION(decays_HO_SM_BSM.partial_width_hh_to_barFeFe(&m, 1, 1), 5.6230325675374227851e-7, 1e-15);
 }
